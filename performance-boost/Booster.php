4c58cf93ac0e8ec82b8821d825a8869e65fcdad2
Pavel Kostadinov <pavelrdks@gmail.com>
2021-01-30 15:34:15 +0200

    add boost_js_extractor

--- performance-boost/Booster.php
+++ performance-boost/Booster.php
@@ -25,6 +25,7 @@ class Booster
     private function boost_optimize_page($html){
 
         var_dump('PAGE OPTIMIZE');
+        $html = self::boost_js_extractor($html);
 
         return $html;
     }
@@ -35,28 +36,35 @@ class Booster
     }
 
     private function boost_js_extractor($html){
-
         try {
-            //code...
+            $optimize_html = $html;
+
             $jspatern = '/<script\b[^>]*>([\s\S]*?)<\/script>/m';
+            // $jspatern = "/\<script(.*?)?\>(.|\\n)*?\<\/script\>/i";
 
-            preg_match_all($jspatern, $html, $matches_js, PREG_SET_ORDER, 0);
-            $scripts = array();
+            preg_match_all($jspatern, $optimize_html, $scripts_tags, PREG_SET_ORDER, 0);
+            $inline_scripts = array();
+            // var_dump($scripts_tags);
 
-            foreach ($matched_js as $key => $script) {
+            foreach ($scripts_tags as $key => $script) {
                 $mod_script = str_replace('script', 'template', $script[0]);
-                $mod_script = str_replace('<template', '<template data-boost-id="boost-script-' . $key . '" ', $mod_script);
+                $mod_script = str_replace('<template', '<template pf-boost-id="boost-script-' . $key . '" ', $mod_script);
                 
                 // getter src attr content.
-                preg_match('/src="(.*?)"/', $mod_script , $srcAttr);
-
+                preg_match('/src=(\'|")(.*?)(\'|")/m', $mod_script , $srcAttr);
+                $srcAttr = $srcAttr[0];
                 if ($srcAttr) {
-
-                } else if ($script){
-
+                    $mod_script = str_replace('src=','pf-boost-src=',$mod_script);
+                } else if ($script[1]){
+                    array_push($inline_scripts, array('pf-boost-id'=> 'boost-script-'. $key,'content'=>$script[1]));
+                    $mod_script = str_replace($script[1], '', $mod_script);
                 }
 
+                $optimize_html = str_replace($script[0],$mod_script, $optimize_html);
+                $optimize_html = str_replace('</head>','<script>var boostInlineScripts=' . json_encode($inline_scripts) . '</script></head>', $optimize_html);
             }
+
+            $html = $optimize_html;
         } catch (\Throwable $th) {
             //throw $th;
         }
